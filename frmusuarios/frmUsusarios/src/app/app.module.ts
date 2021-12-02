6982efcf73f1c0953352f91c03d47c2f384afad1
Cesar Tobias <cesar.tobias@gmail.com>
2021-12-02 11:06:54 -0600

    Se crea  carpeta components

--- frmusuarios/frmUsusarios/src/app/app.module.ts
+++ frmusuarios/frmUsusarios/src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {ReactiveFormsModule} from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UsuarioEditorComponent } from './usuario-editor/usuario-editor.component';
+import { UsuarioEditorComponent } from './components/usuario-editor/usuario-editor.component';
+
 
 @NgModule({
   declarations: [
